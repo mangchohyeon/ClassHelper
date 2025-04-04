4d0b3d1425caa1c02462e152fdfabff520be671d
mangchohyeon <qgeon0419@gmail.com>
2025-04-04 14:00:00 +0900

    vite-plugin-svgr 모듈 다운로드 함.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path' //추가
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve:{ //추가
     alias:[{ find : "@src", replacement : path.resolve(__dirname, "src")},
            { find : "@assets", replacement : path.resolve(__dirname, "src/assets")},
