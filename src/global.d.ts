8690504753126d7efa50813418f5cc14a5889c58
mangchohyeon <qgeon0419@gmail.com>
2025-04-05 08:52:05 +0900

    SVG 모듈 선언 제거

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,10 +2,3 @@ declare module '*.module.css' {
     const content: { [key: string]: string };
     export = content;
 }
-
-declare module "*.svg" {
-    import React from "react";
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-}
