bfade1946c1f74ad03f74944b2843a54956b69b6
mangchohyeon <qgeon0419@gmail.com>
2025-04-05 10:02:12 +0900

    import 방식 수정

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,14 +1,12 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import * as webVitals from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    webVitals.getCLS(onPerfEntry);
+    webVitals.getFID(onPerfEntry);
+    webVitals.getFCP(onPerfEntry);
+    webVitals.getLCP(onPerfEntry);
+    webVitals.getTTFB(onPerfEntry);;
   }
 };
 
