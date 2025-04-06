e10a97a30379c56a507f629c38f347193f50849d
mangchohyeon <qgeon0419@gmail.com>
2025-04-06 17:46:18 +0900

    Table 컴포넌트 작업 중

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,28 +1,31 @@
 import { ComponentsProps } from '@/types/ComponentsProps';
+import { v4 as uuidv4 } from 'uuid';
 
 interface TableProps extends ComponentsProps {
   row: number;
   column: number;
   width?: string;
-  RowStyle?: Array<Record<string, string | number>>;
-  DataStyle? : Array<Record<string, string | number>>;
+  RowProps? : ComponentsProps;
+  TdProps? : ComponentsProps;
+  TdLists : React.ReactNode[][];
 }
 
-
-
 function Table(props : TableProps) {
+
   return (
     <table
     className={props.className}
     id={props.id}
     style={props.style}
-    width={props.width}
     >
-      <tbody>
-        
-      </tbody>
+      {Array.from({length : props.row}).map((_1, RowIndex:number) => (
+        <tr {...props.RowProps}>
+          {Array.from({length : props.column}).map((_2, ColumnIndex : number) => (
+            <td {...props.TdProps}>{props.TdLists[RowIndex][ColumnIndex]}</td>
+          ))}
+        </tr>
+      ))}
     </table>
   )
 }
 
-export default Table;
