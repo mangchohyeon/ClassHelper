852ffcdb26f9b882202b3ede9fb006cbfee4df30
angchohyeon <qgeon0419@gmail.com>
2025-04-09 15:19:35 +0900

    onClick과 children 프로퍼티 추가함.

--- src/types/ComponentsProps.tsx
+++ src/types/ComponentsProps.tsx
@@ -1,6 +1,8 @@
 export interface ComponentsProps {
     className?: string;
     id?: string;
+    onClick? : any;
+    children? : React.ReactNode;
     style?: React.CSSProperties;
     width? : string;
     height? : string;
