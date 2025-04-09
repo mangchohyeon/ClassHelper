7b0fa9ab06408e9d53a83feb0e230904f0923ddc
angchohyeon <qgeon0419@gmail.com>
2025-04-09 15:10:15 +0900

    css 관련하여 수정함.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react';
+import { useEffect } from 'react';
 import Layout from '@layout/Layout';  // 위에서 만든 Layout 컴포넌트
 import Home from '@pages/Home/Home';
 import EngWords from '@pages/EngWords/EngWords';
@@ -8,7 +9,19 @@ import ClassHelper from '@pages/ClassHelper/ClassHelper'
 import ChangeClass from '@pages/ClassHelper/ChangeClass/ChangeClass';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
+
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    let vw = window.innerHeight* 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
+  }
+
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <React.StrictMode>
       <ChakraProvider value={defaultSystem}>
