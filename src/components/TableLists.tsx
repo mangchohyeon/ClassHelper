fc3d667dcb5da739384ffe913998a63fb47d9339
angchohyeon <qgeon0419@gmail.com>
2025-04-09 15:16:13 +0900

    TableLists 컴포넌트는 완성함. 그러나 TableLists컴포넌트를 여러개의 컴포넌트로 나누어야 할 것으로 예상됨.

--- src/components/TableLists.tsx
+++ src/components/TableLists.tsx
@@ -1,58 +1,55 @@
-import styled from 'styled-components';
 import { ComponentsProps } from '@/types/ComponentsProps';
+import { v4 as uuidv4 } from 'uuid';
+import styles from './TableLists.module.css';
 
 interface TableListsProps extends ComponentsProps {
-    row : number;
-    column : number;
-    contentLists : any[][];
+  row: number;
+  column: number;
+  width?: string;
+  RowProps? : ComponentsProps;
+  TdProps? : ComponentsProps;
+  TdLists : React.ReactNode[][];
 }
-
-const TableLines = styled.ul`
-    list-style : none;
-    display : flexbox;
-    flex-direction : column;
-    width : 100%;
-    height : auto;
-`;
-
-const TableLineWrapper = styled.li`
-    display : block;
-    flex-direction : row;
-    width : 100%;
-    height : auto;
-`;
-
-const TableLine = styled.ul`
-    display : flex;
-    flex-direction : row;
-    width : 100%;
-`;
-
-const TableCell = styled.li`
-    display : block;
-    margin-left : 1%;
-    margin-right : 1%;
-`;
 
 function TableLists(props : TableListsProps) {
-    return (
-        <TableLines 
-        className={props.className}
-        id={props.id}
-        style={props.style}>
-            {Array.from({length : props.row}).map((_, LineIndex) => (
-            <TableLineWrapper>
-                <TableLine key={`TableLine${LineIndex}`}>
-                    {Array.from({length : props.column}).map((_2, CellIndex) => (
-                    <TableCell key={`TableCell${LineIndex}-${CellIndex}`}>
-                        {props.contentLists[LineIndex][CellIndex]}
-                    </TableCell>
-                    ))}
-                </TableLine>
-            </TableLineWrapper>
-            ))}
-        </TableLines>
-    )
+  const TableListsClass = `${props.className || styles.TableLists}`;
+
+
+  const RowClass = (props.RowProps?.className || '') + (props.RowProps?.style != undefined 
+  ? ''
+  : styles.TableListsRow);
+
+  const TdClass = (props.TdProps?.className || '') + (props.TdProps?.style != undefined
+  ? ''
+  : styles.TableListsData);
+
+  return (
+    <div 
+    className={TableListsClass}
+    id={props.id}
+    style={props.style}>
+        {Array.from({length : props.row}).map((_1, RowIndex) => (
+            <ul
+            className={RowClass}
+            id={`TableListsRow${RowIndex}`}
+            style={props.RowProps?.style}
+            key={uuidv4()}>
+                {Array.from({length : props.column}).map((_2, ColumnIndex) => (
+                    <li
+                    className={TdClass}
+                    id={`TableListsData${RowIndex}-${ColumnIndex}`}
+                    style={props.TdProps?.style}
+                    key={uuidv4()}>
+                        {props.TdLists[RowIndex][ColumnIndex]}
+                    </li>
+
+                ))}
+            </ul>
+            
+
+        ))}
+    </div>
+  )
 }
 
-export default TableLists;
+export { TableLists, TableListsProps };
