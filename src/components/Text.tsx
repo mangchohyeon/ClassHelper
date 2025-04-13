2ba5c3997843cb19689a16786e170f557f865671
mangchohyeon <qgeon0419@gmail.com>
2025-04-13 21:51:56 +0900

    chakra ui의 text활용한 Text 컴포넌트 생성함.

---
+++ src/components/Text.tsx
@@ -0,0 +1,19 @@
+import { Text as TextTemp, TextProps as TextPropsTemp } from "@chakra-ui/react";
+
+interface TextProps extends TextPropsTemp {
+    className? : string;
+    id? : string;
+    style? : React.CSSProperties;
+}
+
+export function Text(props : TextProps) {
+    return (
+        <text 
+        className={props.className}
+        id={props.id}
+        style={props.style}
+        >
+            {props.children}
+        </text>
+    )
+}
