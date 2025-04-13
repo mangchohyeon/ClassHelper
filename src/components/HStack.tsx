b90c2cb4307203bfe690d9836ebe3a6bd6743a3e
mangchohyeon <qgeon0419@gmail.com>
2025-04-13 21:50:43 +0900

    HStack 컴포넌트 수정함. 기본 props를 StackProps로 수정함.

--- src/components/HStack.tsx
+++ src/components/HStack.tsx
@@ -1,16 +1,15 @@
-import { HStack as Hstack } from "@chakra-ui/react"
+import { HStack as Hstack, StackProps } from "@chakra-ui/react"
 import { ComponentsProps } from "@/types/ComponentsProps"
 
-interface HStackProps extends ComponentsProps {
-  
-}
 
-export function HStack(props : HStackProps) {
+export function HStack(props : StackProps) {
   return (
     <Hstack
     className={props.className}
     id={props.id}
-    style={props.style}>
+    style={props.style}
+    align={props.align}
+    justify={props.justify}>
       {props.children}
     </Hstack>
   )
