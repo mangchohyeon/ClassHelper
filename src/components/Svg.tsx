1402fa6131d1f6a8ff6f94b47893519d14735848
mangchohyeon <qgeon0419@gmail.com>
2025-04-13 21:51:32 +0900

    모든 Svg 파일에 적용될 수 있도록 svg 컴포넌트 생성.

---
+++ src/components/Svg.tsx
@@ -0,0 +1,13 @@
+import { ImgProps } from "@/types/ImgTypes";
+
+export function Svg(props: ImgProps ) {
+    return (
+        <img 
+        className={props.className}
+        id={props.id}
+        style={props.style}
+        src={props.src}
+        alt={props.alt} />
+    )
+}
+
