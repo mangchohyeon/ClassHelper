9111514a8df205758dfc6b7fb044524791389cc8
mangchohyeon <qgeon0419@gmail.com>
2025-04-14 17:38:29 +0900

    tooltip 컴포넌트 생성, trigger css 수정해야함

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,38 +1,34 @@
-import { default as TooltipTemp } from '@mui/material/Tooltip';
-import { ComponentsProps } from '@/types/ComponentsProps';
-import { Button } from '@mui/material';
+import { Tooltip as tooltip } from "@ark-ui/react";
+import styles from './Tooltip.module.css';
 
-interface TooltipProps extends ComponentsProps {
-    title? : string;
+interface TooltipProps {
+    children? : React.ReactNode;
+    content? : string;
     arrow? : boolean;
-    position? : "top-start" | "top-end" | "top" |
-    "bottom-start" | "bottom-end" | "bottom" |
-    "right-start" | "right-end" | "right" |
-    "left-start" | "left-end" | "left" ;
+    openDelay? : number;
+    closeDelay? : number;
+    placement? : "top" | "top-start" | "top-end" |
+    "bottom" | "bottom-start" | "bottom-end" |
+    "right" | "right-start" | "right-end" |
+    "left" | "left-start" | "left-end";
 }
 
 export function Tooltip(props : TooltipProps) {
     return (
-      <TooltipTemp
-      className={props.className}
-      id={props.id}
-      style={props.style}
-      title={props.title}
-      placement={props.position}
-        slotProps={{
-            popper: {
-            modifiers: [
-                {
-                    name: 'offset',
-                    options: {
-                        offset: [0, -14],
-                    },
-                },
-            ],
-        },
-    }}
-    >
-        {props.children as React.ReactElement}
-    </TooltipTemp>
-    );
-  }
+    <tooltip.Root 
+    openDelay={props.openDelay ? props.openDelay: 0}
+    closeDelay={props.closeDelay ? props.openDelay : 0}>
+        <tooltip.Trigger
+        className={styles.Trigger}>{props.children}</tooltip.Trigger>
+        <tooltip.Positioner>
+            {props.arrow
+            ?<tooltip.Arrow>
+                <tooltip.ArrowTip />
+            </tooltip.Arrow>
+            : <></>}
+            <tooltip.Content>{props.content}</tooltip.Content>
+            </tooltip.Positioner>
+    </tooltip.Root>
+    )
+  
+}
