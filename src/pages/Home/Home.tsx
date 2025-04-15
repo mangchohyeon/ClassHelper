8022d6c1932390083974ebad6b479bd7a85ec6d8
mangchohyeon <qgeon0419@gmail.com>
2025-04-15 23:28:25 +0900

    홈 화면 꾸미는 중

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,20 @@
 import styles from './Home.module.css'
-import TextSection from '@components/TextSection'
-import HBlock from '@components/HBlock'
-
+import { Text } from '@components/Text'
 
 function Home() {
     return (
         <main className={styles.main}>
-            <TextSection className={styles.Intro}
-            style={{fontSize : "1.rem"}}>
-                <HBlock 
-                num={3} 
-                className="IntroSubTitle"
-                style={{fontSize:"2em"}}>ClassHelper란?</HBlock> <br />
-                개인적으로 학생이 운영하고 있는 사이트입니다!
-                자리 바꾸기, 팀 나누기 등 선생님들을 위한 페이지들을 개발하고 있습니다. <br />
-                또한 영단어 학습등 클래스 카드와 비슷한 기능들을 개발하고 있습니다. <br />
-                현재는 학교 도우미에서 자리 바꾸기 페이지만 개발해 놓은 상태입니다!
-            </TextSection>
+            <section
+            className={styles.Heading}>
+                ClassHelper란 선생님들 및 학생들의 
+                간편한 활동을 위해 만든 사이트입니다.
+                <Text 
+                style={{
+                    color : '52525B'
+                }}>
+                    ClassHelper를 통해 간편하고
+                </Text>
+            </section>
         </main>
     )
 }
