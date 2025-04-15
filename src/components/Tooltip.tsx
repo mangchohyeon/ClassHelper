4a0f6186945460f7e5149df1648ec39168f32032
mangchohyeon <qgeon0419@gmail.com>
2025-04-15 23:14:15 +0900

    default값으로 배경은 검은색, 글씨는 하얀색으로 설정 padding은 8px, border-radius 4px로 설정

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,9 +1,10 @@
 import { Tooltip as ArkTooltip } from '@ark-ui/react';
 import { ComponentsProps } from '@/types/ComponentsProps';
+import './Tooltip.module.css';
 
 interface TooltipProps extends ComponentsProps {
   children?: React.ReactNode; // 자식 요소
-  content?: string; // 툴팁에 표시할 메시지
+  content?: React.ReactNode; // 툴팁에 표시할 메시지
   arrow?: boolean; // 화살표 표시 여부
   openDelay?: number; // hover 후 툴팁이 나타날 때까지의 지연 시간 (ms)
   closeDelay?: number; // hover가 끝난 후 툴팁이 사라질 때까지의 지연 시간 (ms)
@@ -37,7 +38,8 @@ export function Tooltip(props : TooltipProps) {
             {props.children}
         </ArkTooltip.Trigger>
         {/* 툴팁의 위치와 내용을 정의 */}
-        <ArkTooltip.Positioner>
+        <ArkTooltip.Positioner
+        >
             <ArkTooltip.Content>
                 {/* arrow가 true일 때만 화살표 렌더링 */}
                 {Arrow && <ArkTooltip.Arrow />}
