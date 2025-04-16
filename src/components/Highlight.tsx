e6730d0b94f4e986e1f9b9c7e9cf1e2e1cab67ff
mangchohyeon <qgeon0419@gmail.com>
2025-04-16 00:56:39 +0000

    텍스트 꾸며주는 Highlight컴포넌트 생성함.

---
+++ src/components/Highlight.tsx
@@ -0,0 +1,19 @@
+import { Highlight as HighLight} from "@ark-ui/react";
+import { ComponentsProps } from "@/types/ComponentsProps";
+
+interface HighlightProps extends Omit<ComponentsProps, "width" | "height" | "onClick" | "children"> {
+    query : string | string[];
+    matchAll? : boolean;
+    children : string;
+}
+
+export function Highlight(props : HighlightProps) {
+    return (
+        <HighLight
+        className={props.className}
+        id={props.id}
+        query={props.query}
+        text={props.children}
+        matchAll={props.matchAll} />
+    )
+}
