2096e345f7e6ff04cb55fa2fce28433dfe1daae0
mangchohyeon <qgeon0419@gmail.com>
2025-04-17 02:57:22 +0000

    가독성 변경

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,33 @@
+import { Field, Textarea as TextArea } from "@chakra-ui/react";
+import { ComponentsProps } from '@/types/ComponentsProps';
+
+interface TextareaProps extends ComponentsProps {
+    Label? : React.ReactNode;
+    HelperText? : React.ReactNode;
+    placeholder? : string;
+    variant? : "outline" | "subtle" | "flushed";
+    size? : "xs" | "sm" | "md" | "lg" | "xl";
+    ref? : React.RefObject<any>;
+}
+
+export function Textarea(props : TextareaProps) {
+    return (
+    <Field.Root required>
+        {props.Label != undefined 
+        ? <Field.Label>
+        Comment <Field.RequiredIndicator />
+    </Field.Label>
+    : null}
+        <TextArea 
+        placeholder={props.placeholder}
+        variant={props.variant}
+        resize={"vertical"}
+        ref={props.ref}
+        />
+        {props.HelperText != undefined
+        ? <Field.HelperText>{props.HelperText}</Field.HelperText>
+        : null}
+    </Field.Root>  
+      )
+}
+
