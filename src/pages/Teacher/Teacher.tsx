d1d7c79dccee13a24377a1893a9538df80c59aee
mangchohyeon <qgeon0419@gmail.com>
2025-04-20 22:38:31 +0900

    선생님 페이지 관련 작업 중

--- src/pages/Teacher/Teacher.tsx
+++ src/pages/Teacher/Teacher.tsx
@@ -1,13 +1,21 @@
-import styles from './Teacher.module.css'
-import HBlock from '@components/HBlock'
+import styles from './Teacher.module.css';
+import HBlock from '@components/HBlock';
+import Naviagation2 from '@layout/Naviagion2';
 
 export default function Teacher() {
     return (
         <main className={styles.main}>
-            <section className={styles.Intro}>
-                <HBlock num={2}> 학교 도우미란?</HBlock>
-                
-            </section>
+            <Naviagation2 OnMenuList={[false, true]} />
+
+            <header
+            className={styles.Header}>
+                <div
+                className={styles.HeaderTitle}>
+                    선생님 메뉴란?
+                </div>
+
+                선생님들이 사용할 수 있는 
+            </header>
         </main>
     )
 }
