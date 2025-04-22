2782ac1e4a69d9be9ff575c7adcdcae7b74dd695
mangchohyeon <qgeon0419@gmail.com>
2025-04-22 09:04:06 +0000

    소개 부분 꾸미는 중.

--- src/pages/Teacher/Teacher.tsx
+++ src/pages/Teacher/Teacher.tsx
@@ -1,11 +1,14 @@
 import styles from './Teacher.module.css';
 import HBlock from '@components/HBlock';
 import TeacherNavy from './TeacherNavy';
-import Card from '@components/Card';
+import { Card, HCard } from '@components/Card';
 import Text from '@components/Text';
 import Svg from '@components/Svg';
 import ChangeClassIcon from '@assets/ChangeClassIcon.svg';
 import { Link } from '@components/Link';
+import DivideTeamIcon from '@assets/DivideTeamIcon.svg';
+import HighlightText from '@components/MyHighlightText';
+import ButtonLink from '@components/ButtonLink';
 
 export default function Teacher() {
     return (
@@ -15,31 +18,32 @@ export default function Teacher() {
 
             <header
             className={styles.Header}>
-                <div
-                className={styles.HeaderTitle}>
-                    선생님 메뉴란?
-                </div>
-                선생님들이 사용할 수 있도록 만든 페이지입니다.
+                <HighlightText className={styles.Title1}>선생님 메뉴란?</HighlightText> <br />
+                선생님들이 필요로 하시는 기능을 모아 페이지입니다!
             </header>
 
             <section
             className={styles.SubmenuIntroduceSection}>
-                <Text
-                className={styles.SubmenuIntroduceSectionTitle}>
-                    선생님 페이지 메뉴
-                </Text>
+                <HighlightText className={styles.Title1}> 주요 기능 </HighlightText>
 
                 {/**자리바꾸기 카드 */}
-                <Card
+                <HCard
                 className={styles.SubmenuCard}
-                Img={<Svg
-                    className={styles.SubmenuCardImg}
-                    src={ChangeClassIcon}
-                    />}
+                Img={<div
+                    className={styles.SubmenuCardImgWrapper}>
+                        <Svg
+                        className={styles.SubmenuCardImg}
+                        src={ChangeClassIcon}
+                        />
+                    </div>}
+
+                BoxProps={{
+                    className : styles.SubmenuCardContent
+                }}
 
                 Title={<Text
                     className={styles.SubmenuCardTitle}>
-                        자리 바꾸기
+                        자리 바꾸기 
                     </Text>}
 
                 Description={<section
@@ -48,20 +52,30 @@ export default function Teacher() {
                         자리 배치를 선택하면 <br />
                         학생들의 자리를 간편하게 바꿀수 있습니다!<br />
                         <Text>Excel파일에는 첫번째 세로줄에 학생명단이 있어야 합니다.</Text>
+                        <section
+                        className={styles.SubmenuCardFooter}>
+                            <ButtonLink
+                            className={styles.SubmenuCardButtonLink}>
+                                <Link to="/teacher/changeclass">
+                                <Text>바로가기</Text>
+                                </Link>
+                            </ButtonLink>
+                        </section>
                     </section>}
 
-                Footer={<Link to="/teacher/changeclass">
-                    <Text>자리 바꾸기 바로가기</Text>
-                </Link>}
+        
                 />
             
             {/**팀 나누기 카드 */}
-            <Card
+            <HCard
             className={styles.SubmenuCard}
-            Img={<Svg
-                className={styles.SubmenuCardImg}
-                src={ChangeClassIcon}
-                />}
+            Img={<div
+                className={styles.SubmenuCardImgWrapper}>
+                    <Svg
+                    className={styles.SubmenuCardImg}
+                    src={DivideTeamIcon}
+                    />
+                </div>}
 
             Title={<Text
                 className={styles.SubmenuCardTitle}>
