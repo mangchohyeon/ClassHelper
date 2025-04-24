6b4b083be2752592b6a9a6e16762b2ccc8d0fe1a
mangchohyeon <qgeon0419@gmail.com>
2025-04-24 03:26:26 +0000

    shuffle 함수 업데이트 함. 성능은 업대아트한 버전이 더 우수함.

--- src/utils/shuffle.tsx
+++ src/utils/shuffle.tsx
@@ -1,3 +1,7 @@
 export function shuffle(array : any[]) {
-    array.sort(() => Math.random() - 0.5);
-}
+    for (let i = array.length - 1; i > 0; i--) {
+        // 무작위로 index 값 생성 (0 이상 i 미만)
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
