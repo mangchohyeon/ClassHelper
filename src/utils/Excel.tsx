fdc95b23b87df675ec488af4fd3e41fe26cb185c
mangchohyeon <qgeon0419@gmail.com>
2025-04-25 09:05:34 +0000

    비동기 처리를 동기로 바꿈

--- src/utils/Excel.tsx
+++ src/utils/Excel.tsx
@@ -1,30 +1,26 @@
 import * as XLSX from 'xlsx';
 
-async function getFirstColumn(file: File | null | undefined): Promise<any[]> {
+function getFirstColumn(file: File | null | undefined) {
   if (file == null) {
     return [null];
   }
-  try {
-    const arrayBuffer = await file.arrayBuffer();
-    const workbook = XLSX.read(arrayBuffer, { type: 'array' });
-    const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    let row = 1;
-    let result: any[] = [];
-    while (true) {
-      const cellAddress = `A${row}`;
-      const cell = sheet[cellAddress];
-      if (cell && cell.v != null && cell.v !== '') {
-        result.push(cell.v);
-        row++;
-      } else {
-        break;
-      }
+
+  const arrayBuffer = file.arrayBuffer();
+  const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  let row = 1;
+  let result: any[] = [];
+  while (true) {
+    const cellAddress = `A${row}`;
+    const cell = sheet[cellAddress];
+    if (cell && cell.v != null && cell.v !== '') {
+      result.push(cell.v);
+      row++;
+    } else {
+      break;
     }
-    return result;
-  } catch (error) {
-    console.error('Error processing file:', error);
-    return [null];
   }
+  return result;
 };
 
 export default getFirstColumn;
