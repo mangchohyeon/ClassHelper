a07834e8b2d7ca9068f96744225a1019252a70e1
mangchohyeon <qgeon0419@gmail.com>
2025-05-10 22:58:24 +0900

    꼭 필요한 Properties 제외한 나머지 Properties들은 삭제함.

--- src/types/ComponentsProps.tsx
+++ src/types/ComponentsProps.tsx
@@ -1,11 +1,8 @@
 export default interface ComponentsProps {
     className?: string;
     id?: string;
-    onClick? : any;
     children? : React.ReactNode;
     style?: React.CSSProperties;
-    width? : string;
-    height? : string;
 }
 
 export { ComponentsProps } ;
